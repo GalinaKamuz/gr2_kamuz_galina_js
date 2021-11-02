9491ce838472c410a63bf2bb209333bc736e9e02
GalinaKamuz <93265525+GalinaKamuz@users.noreply.github.com>
2021-11-02 18:01:14 +0200

    Add files via upload

---
+++ js/homework_1.js
@@ -0,0 +1,24 @@
+let first_name = 'Галина';
+let last_name = 'Камуз';
+const BIRTH_YEAR = 1991;
+const BIRTH_TOWN = 'Торецк';
+const EYE_COLOR = 'зелёный';
+const TEMPERAMENT = 'флегматик';
+let my_pet = 'кот';
+let pet_age = 17;
+let working = false;
+let studying = true;
+let it_experience = false;
+
+
+alert ('Всем привет! Меня зовут ' + first_name + ' ' + last_name +'. '+
+		'Я родилась в ' + BIRTH_YEAR + ' году, в городе ' + BIRTH_TOWN +'. ' );
+alert ('Остальные переменные и константы я вывела в консоль');
+
+console.log('EYE_COLOR', EYE_COLOR, typeof(EYE_COLOR));
+console.log('TEMPERAMENT', TEMPERAMENT, typeof(TEMPERAMENT));
+console.log('my_pet', my_pet, typeof(my_pet));
+console.log('pet_age', pet_age, typeof(pet_age));
+console.log('working', working, typeof(working));
+console.log('studying', studying, typeof(studying));
+console.log('it_experience', it_experience, typeof(it_experience));
