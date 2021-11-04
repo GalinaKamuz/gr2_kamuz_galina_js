ca78141e9bad2d45f36dadc715a60829802745bf
GalinaKamuz <93265525+GalinaKamuz@users.noreply.github.com>
2021-11-04 18:42:39 +0200

    Update homework_4_4.js

--- js/homework_4_4.js
+++ js/homework_4_4.js
@@ -5,16 +5,48 @@
 Если не совпадает выдаем сообщение «Игра окончена. 
 Ваши очки: {кол-во очков}».*/
 
-let score = 0;
+function start_game_city_chain ( start_city = 'Киев') {
+	if (typeof(start_city)!== 'string') {
+		return null;
+	}
 
-alert ('Игра в города!');
-let previous_town = prompt('Введите первый город');
-let next_town = previous_town;
+	alert (`Игра началась. Первый город ${start_city}`)
 
-while ((next_town[0])===(previous_town[previous_town.length - 1])) {
-	next_town = prompt('Введите город');
-	score+=1;
-	previous_town = next_town;
+	let score = 0;
+	let previos_cities = [start_city];
+
+	while (true) {
+		let new_city = prompt('Введите город');
+
+		if (previos_cities.indexOf(new_city) !== -1) {
+			alert (`Такой город уже был`);
+			continue;
+		}
+
+		let prev_city = previos_cities[previos_cities.length - 1];
+
+		let last_char = prev_city[prev_city.length - 1];
+		let first_char = new_city[0];
+
+		last_char = last_char.toLowerCase();
+		first_char = first_char.toLowerCase();
+
+		if (last_char === 'ь' || last_char === 'ы' ) {
+			let last_char = prev_city[prev_city.length - 2];
+			last_char = last_char.toLowerCase();
+		}
+
+		if (last_char === first_char) {
+			score+=1;
+		} else {
+			alert (`Игра окончена. Ваши очки: ${score}`);
+			return score;
+		}
+
+		previos_cities.push(new_city);
+	
+	}
+	return score;
+	
 }
-
-alert (`Игра окончена. Ваши очки: ${score}`);
+start_game_city_chain ();
