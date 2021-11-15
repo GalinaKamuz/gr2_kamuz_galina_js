496f3c84c2f365d66cabbe4f63147cb3a2cf2237
GalinaKamuz <93265525+GalinaKamuz@users.noreply.github.com>
2021-11-16 00:20:34 +0200

    Update homework_5_10.js

--- js/homework_5_10.js
+++ js/homework_5_10.js
@@ -1,21 +1,19 @@
-/*Напишите функцию get_min( arr ), которая находит 
-минимальное значение в заданном массиве.*/
+/*Напишите функцию get_random( arr ), 
+которая вернет случайный элемент входящего массива arr.*/
 
-function get_min(arr) {
-
+function get_random(arr) {
 	if (!Array.isArray(arr)) {
 		return null;
 	}
 
-let result_min = Infinity;
+	let index = 0;
+	let temp = Math.random();
+	
+	index = Math.floor(arr.length * temp);
+	let result = arr[index];
 
-for (let item of arr) {
-	if (item < result_min) {
-		result_min = item
-	}
-}
-return result_min;
+	return result;
 }
 
-let array = [3,8,5,6,2,4,7,8];
-console.log(get_min(array));
+let array = [27,9,44,2,151,12,7,85,1,13];
+console.log(get_random(array));
